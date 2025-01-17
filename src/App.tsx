b2fdceb9c22943af447d4690dd3ca7adc2256124
21subham <subhamrajbhandari@gmail.com>
2025-01-17 15:22:25 +0545

    type presets

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Homepage from "./components/Homepage";
 import FileDisplay from "./components/FileDisplay";
 import Information from "./components/Information";
 import Transcribing from "./components/Transcribing";
+import { MessageTypes } from "./utils/presets";
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
@@ -12,6 +13,7 @@ function App() {
   const [output, setOutput] = useState<Blob | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [finished, setFinished] = useState<boolean>(false);
+  const [downloading, setDownloading] = useState<boolean>(false);
 
   const isAudioAvailable = file || audioStream;
 
@@ -19,9 +21,63 @@ function App() {
 
   useEffect(() => {
     if (!worker.current) {
-      worker.current = new worker(new URL("./"));
+      worker.current = new Worker(
+        new URL("./utils/whisper.worker.ts", import.meta.url),
+        { type: "module" }
+      );
     }
-  });
+
+    const onMessageReceived = async (e: any) => {
+      switch (e.data.type) {
+        case "DOWNLOADING":
+          setDownloading(true);
+          console.log("downloading");
+          break;
+
+        case "LOADING":
+          setIsLoading(true);
+          console.log("loading");
+          break;
+
+        case "RESULT":
+          setOutput(e.data.results);
+          break;
+
+        case "INFERENCE_DONE":
+          setFinished(true);
+          console.log("done");
+
+          break;
+      }
+    };
+    worker.current.addEventListener("message", onMessageReceived);
+    return () =>
+      worker.current?.removeEventListener("message", onMessageReceived);
+  }, []);
+
+  async function readAudioFrom(file) {
+    const sampling_rate = 1600;
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+    const response = await file.arrayBuffer();
+    const decoded = await audioCTX.decodeAudioData(response);
+    const audio = decoded.getChannelData(0);
+    return audio;
+  }
+
+  async function handleFormSubmission() {
+    if (!file && !audioStream) return;
+
+    let audio = await readAudioFrom(file ? file : audioStream);
+    const modal_name = "openai / whisper - tiny.en";
+
+    if (worker.current) {
+      worker.current.postMessage({
+        type: MessageTypes.INFERENCE_REQUEST,
+        audio,
+        modal_name,
+      });
+    }
+  }
 
   function handleAudioReset() {
     setFile(null);
---
+++ src/utils/whisper.worker.ts
@@ -0,0 +1,1 @@
+import { Pipeline } from "@xenova/transformers";
