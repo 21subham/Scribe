434dac37bf70eb1649a9d37f5bd4ccaa94fd859c
21subham <subhamrajbhandari@gmail.com>
2025-01-18 11:33:40 +0545

    whisper worker(xenova)

--- src/utils/whisper.worker.ts
+++ src/utils/whisper.worker.ts
@@ -1,1 +1,14 @@
-import { Pipeline } from "@xenova/transformers";
+import { pipeline } from "@xenova/transformers";
+
+class MyTranscriptPipeline {
+  static task = "automatic-speech-recognition";
+  static model = "openai/whisper-tiny.en";
+  static instance = null;
+
+  static async getInstance(progress_callback = null) {
+    if (this.instance === null) {
+      this.instance = await pipeline(this.task, null, { progress_callback });
+    }
+    return this.instance;
+  }
+}
