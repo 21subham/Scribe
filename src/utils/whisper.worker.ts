3332842e0de0edb535fe11d5a13724d67bcfb945
21subham <subhamrajbhandari@gmail.com>
2025-01-19 13:45:50 +0545

    worker done

--- src/utils/whisper.worker.ts
+++ src/utils/whisper.worker.ts
@@ -10,25 +10,27 @@ class MyTranscriptionPipeline {
     if (this.instance === null) {
       this.instance = await pipeline(this.task, null, { progress_callback });
     }
+
     return this.instance;
   }
 }
 
 self.addEventListener("message", async (event) => {
   const { type, audio } = event.data;
-  if (type == MessageTypes.INFERENCE_REQUEST) {
+  if (type === MessageTypes.INFERENCE_REQUEST) {
     await transcribe(audio);
   }
 });
 
 async function transcribe(audio) {
-  sendLoadingMessage("Loading");
+  sendLoadingMessage("loading");
 
   let pipeline;
+
   try {
     pipeline = await MyTranscriptionPipeline.getInstance(load_model_callback);
-  } catch (error) {
-    console.log("pipeline error", error);
+  } catch (err) {
+    console.log(err.message);
   }
 
   sendLoadingMessage("success");
@@ -36,14 +38,14 @@ async function transcribe(audio) {
   const stride_length_s = 5;
 
   const generationTracker = new GenerationTracker(pipeline, stride_length_s);
-  await pripeline(audio, {
+  await pipeline(audio, {
     top_k: 0,
     do_sample: false,
     chunk_length: 30,
     stride_length_s,
     return_timestamps: true,
-    callback_fnction:
-      generationTracker.callbackFunction.blind(generationTracker),
+    callback_function:
+      generationTracker.callbackFunction.bind(generationTracker),
     chunk_callback: generationTracker.chunkCallback.bind(generationTracker),
   });
   generationTracker.sendFinalResult();
@@ -85,5 +87,84 @@ class GenerationTracker {
     this.processed_chunks = [];
     this.callbackFunctionCounter = 0;
   }
+
+  sendFinalResult() {
+    self.postMessage({ type: MessageTypes.INFERENCE_DONE });
+  }
+
+  callbackFunction(beams) {
+    this.callbackFunctionCounter += 1;
+    if (this.callbackFunctionCounter % 10 !== 0) {
+      return;
+    }
+
+    const bestBeam = beams[0];
+    let text = this.pipeline.tokenizer.decode(bestBeam.output_token_ids, {
+      skip_special_tokens: true,
+    });
+
+    const result = {
+      text,
+      start: this.getLastChunkTimestamp(),
+      end: undefined,
+    };
+
+    createPartialResultMessage(result);
+  }
+
+  chunkCallback(data) {
+    this.chunks.push(data);
+    const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(
+      this.chunks,
+      {
+        time_precision: this.time_precision,
+        return_timestamps: true,
+        force_full_sequence: false,
+      }
+    );
+
+    this.processed_chunks = chunks.map((chunk, index) => {
+      return this.processChunk(chunk, index);
+    });
+
+    createResultMessage(
+      this.processed_chunks,
+      false,
+      this.getLastChunkTimestamp()
+    );
+  }
+
+  getLastChunkTimestamp() {
+    if (this.processed_chunks.length === 0) {
+      return 0;
+    }
+  }
+
+  processChunk(chunk, index) {
+    const { text, timestamp } = chunk;
+    const [start, end] = timestamp;
+
+    return {
+      index,
+      text: `${text.trim()}`,
+      start: Math.round(start),
+      end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s),
+    };
+  }
 }
-1+ 1 = 5
+
+function createResultMessage(results, isDone, completedUntilTimestamp) {
+  self.postMessage({
+    type: MessageTypes.RESULT,
+    results,
+    isDone,
+    completedUntilTimestamp,
+  });
+}
+
+function createPartialResultMessage(result) {
+  self.postMessage({
+    type: MessageTypes.RESULT_PARTIAL,
+    result,
+  });
+}
