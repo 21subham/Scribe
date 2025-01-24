03df0bf47ad05eded1ab1b0ac9eea4c82e8477a7
21subham <subhamrajbhandari@gmail.com>
2025-01-24 10:23:19 +0545

    fix type errors

--- src/utils/translate.worker.ts
+++ src/utils/translate.worker.ts
@@ -1,12 +1,12 @@
-import { pipeline, env } from "@xenova/transformers";
+import { pipeline, env, PipelineType } from "@xenova/transformers";
 
 env.allowLocalModels = false;
 class MyTranslationPipeline {
-  static task = "translation";
+  static task: PipelineType = "translation";
   static model = "Xenova/nllb-200-distilled-600M";
   static instance: any = null;
 
-  static async getInstance(progress_callback: null) {
+  static async getInstance(progress_callback: (progress: any) => void) {
     if (this.instance === null) {
       this.instance = await pipeline(this.task, this.model, {
         progress_callback,
@@ -17,13 +17,20 @@ class MyTranslationPipeline {
 }
 
 self.addEventListener("message", async (event: MessageEvent) => {
+  // Define the structure of the event data
+  const data = event.data as {
+    text: string;
+    tgt_language: string;
+    src_language: string;
+  };
+
   let translator = await MyTranslationPipeline.getInstance((x) => {
     self.postMessage(x);
   });
   let output = await translator(event.data.text, {
-    tgt_lang: event.data.tgt_language,
-    src_lang: event.data.src_language,
-    callback_function: (x) => {
+    tgt_lang: data.tgt_language,
+    src_lang: data.src_language,
+    callback_function: (x: any) => {
       self.postMessage({
         status: "update",
         output: translator.tokenizer.decode(x[0].output_token_ids, {
